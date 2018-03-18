2281e14d2f2610f73ec1231b0bcb5c1f270a43a5
fkfac <37439722+fkfac@users.noreply.github.com>
2018-03-18 12:55:22 +0530

    Update functions.py
    1. Rectified the indentation errors.
    2. Also added the multiply function.

--- functions.py
+++ functions.py
@@ -2,13 +2,14 @@ print("The main Funtctions")
 
 def add(a, b):
   c = a + b
-  return c
+  return (c)
   
- def multiply(a, b):
+def multiply(a, b):
+  c = a * b
+  return (c)
+  
+def sub(a, b):
   c = a + b
   
- def sub(a, b):
-  c = a + b
-  
- def divide(a, b):
+def divide(a, b):
   c = a + b
