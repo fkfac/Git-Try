7c6058b0cd4ae5681319365890243e598bb48407
fkfac <37439722+fkfac@users.noreply.github.com>
2018-03-18 12:52:12 +0530

    Create functions.py
    Just a test file to understand  how to incorporate changes .

---
+++ functions.py
@@ -0,0 +1,14 @@
+print("The main Funtctions")
+
+def add(a, b):
+  c = a + b
+  return c
+  
+ def multiply(a, b):
+  c = a + b
+  
+ def sub(a, b):
+  c = a + b
+  
+ def divide(a, b):
+  c = a + b
