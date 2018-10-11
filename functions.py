25f821b89dd609fef67d30f87544466af87df5a1
fedrick <37439722+fkfac@users.noreply.github.com>
2018-10-12 02:56:05 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,14 +3,3 @@ print("The main Funtctions")
 def add(a, b):
   c = a + b
   return (c)
-  
-def multiply(a, b):
-  c = a * b
-  return (c)
-  
-def sub(a, b):
-  c = a - b
-  return (c)
-  
-def divide(a, b):
-  c = a + b
